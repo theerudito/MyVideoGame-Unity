c622c6dd0a43c5a0072714ad2834ad7b54a88272
theerudito <erudito.tv@gmail.com>
2023-06-11 15:28:15 -0500

    v2

--- Assets/Scenes/menu/ScriptMenu.cs
+++ Assets/Scenes/menu/ScriptMenu.cs
@@ -2,57 +2,87 @@ using UnityEngine;
 
 public class ScriptMenu : MonoBehaviour
 {
-  [SerializeField] private AudioClip soundMenu;
-  [SerializeField] private AudioSource musicSource;
-  [SerializeField] private GameObject btnBurgerMenu;
-  [SerializeField] private GameObject btnPanelMenu;
-  [SerializeField] private GameObject btnPause;
-  [SerializeField] private GameObject btnPlay;
-  private bool isMusicPlaying = true;
-  [SerializeField] private Animator sceneAnimation;
+    [SerializeField]
+    public AudioClip soundClipMain;
+    public AudioClip soundClipSecond;
 
-  void Awake()
-  {
-    btnPanelMenu.SetActive(false);
-    musicSource.Play();
-    sceneAnimation.SetTrigger("fadeOut");
-  }
+    [SerializeField]
+    private AudioSource mainAudioSource;
 
-  public void NextScene(int scene)
-  {
-    ManagerScene.Instance.LoadSceneManager(scene);
-  }
-  public void PauseThemeHome()
-  {
-    if (isMusicPlaying)
+    [SerializeField]
+    private AudioSource secondaryAudioSource;
+
+    [SerializeField]
+    private GameObject btnBurgerMenu;
+
+    [SerializeField]
+    private GameObject btnPanelMenu;
+
+    [SerializeField]
+    private GameObject btnPause;
+
+    [SerializeField]
+    private GameObject btnPlay;
+    private bool isMusicPlaying = true;
+
+    [SerializeField]
+    private Animator sceneAnimation;
+
+    private void Awake()
     {
-      isMusicPlaying = false;
-      btnPlay.SetActive(false);
-      btnPause.SetActive(true);
-      musicSource.Pause();
+        btnPanelMenu.SetActive(false);
+        mainAudioSource.Play();
+        sceneAnimation.SetTrigger("fadeOut");
+        secondaryAudioSource = gameObject.AddComponent<AudioSource>();
     }
-    else
-    {
-      isMusicPlaying = true;
-      btnPause.SetActive(false);
-      btnPlay.SetActive(true);
-      musicSource.UnPause();
-    }
-  }
-  public void ExitGame()
-  {
-    Application.Quit();
-    Debug.Log("Exit");
-  }
 
-  public void ShowBurger()
-  {
-    btnBurgerMenu.SetActive(false);
-    btnPanelMenu.SetActive(true);
-  }
-  public void CloseBurger()
-  {
-    btnBurgerMenu.SetActive(true);
-    btnPanelMenu.SetActive(false);
-  }
+    public void NextScene(int scene)
+    {
+        ManagerScene.Instance.LoadSceneManager(scene);
+    }
+
+    public void ClickRj45()
+    {
+        if (soundClipSecond != null)
+        {
+            secondaryAudioSource.clip = soundClipSecond;
+            secondaryAudioSource.Play();
+        }
+    }
+
+    public void PauseThemeHome()
+    {
+        if (isMusicPlaying)
+        {
+            isMusicPlaying = false;
+            btnPlay.SetActive(false);
+            btnPause.SetActive(true);
+            mainAudioSource.Pause();
+        }
+        else
+        {
+            isMusicPlaying = true;
+            btnPause.SetActive(false);
+            btnPlay.SetActive(true);
+            mainAudioSource.UnPause();
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+
+    public void ShowBurger()
+    {
+        btnBurgerMenu.SetActive(false);
+        btnPanelMenu.SetActive(true);
+    }
+
+    public void CloseBurger()
+    {
+        btnBurgerMenu.SetActive(true);
+        btnPanelMenu.SetActive(false);
+    }
 }
